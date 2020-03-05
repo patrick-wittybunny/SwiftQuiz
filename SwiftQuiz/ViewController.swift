dfec91ed666183002b8e26cceeed4f0261f9241a
Patrick <patrick.wittybunny@gmail.com>
2020-03-05 13:10:16 +0800

    Initial Commit

---
+++ SwiftQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftQuiz
+//
+//  Created by Patrick Domingo on 3/5/20.
+//  Copyright © 2020 Patrick Domingo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
