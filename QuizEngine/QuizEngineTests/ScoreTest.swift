fea977153c85cec69da581640fb17e123d8107af
Patrick <patrick.wittybunny@gmail.com>
2020-03-12 14:31:17 +0800

    allow scoring with unequal sized data

--- QuizEngine/QuizEngineTests/ScoreTest.swift
+++ QuizEngine/QuizEngineTests/ScoreTest.swift
@@ -35,11 +35,18 @@ class ScoreTest: XCTestCase {
         XCTAssertEqual(score, 2)
     }
     
+    func test_withUnequalSizeData() {
+        let score = BasicScore.score(for: ["correct", "correct 2", "an extra answer"], comparingTo: ["correct", "correct 2"])
+        
+        XCTAssertEqual(score, 2)
+    }
+    
     private class BasicScore {
         static func score(for answers: [String], comparingTo correctAnswers:[String] = []) -> Int {
             if answers.isEmpty { return 0 }
             var score = 0
             for (index, answer) in answers.enumerated() {
+                if index >= correctAnswers.count { return score }
                 score += (answer == correctAnswers[index]) ? 1 : 0
             }
             return score
