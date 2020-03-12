72b1b9fe228f46842992409753d14c41aa3fb97d
Patrick <patrick.wittybunny@gmail.com>
2020-03-12 18:41:07 +0800

    clean up private implementation after deprecating code api

--- SwiftQuiz/Main/iOSViewControllerFactory.swift
+++ SwiftQuiz/Main/iOSViewControllerFactory.swift
@@ -10,28 +10,27 @@ import Foundation
 import UIKit
 import QuizEngine
 
-class iOSViewControllerFactory: ViewControllerFactory {
+final class iOSViewControllerFactory: ViewControllerFactory {
     typealias Answers = [(question: Question<String>, answers: [String])]
     
-    private let questions: [Question<String>]
     private let options: Dictionary<Question<String>, [String]>
-    private let correctAnswers: () -> Answers
+    private let correctAnswers: Answers
+    
+    private var questions: [Question<String>] {
+        return correctAnswers.map { $0.question }
+    }
     
     init(options: Dictionary<Question<String>, [String]>, correctAnswers: Answers) {
-        self.questions = correctAnswers.map { $0.question }
         self.options = options
-        self.correctAnswers = { correctAnswers }
+        self.correctAnswers = correctAnswers
     }
     
     func questionViewController(for question: Question<String>, answerCallback: @escaping ([String]) -> Void) -> UIViewController {
-        
         guard let options = self.options[question] else {
             fatalError("Couldn't find options for question: \(question)")
         }
         
-        return questionViewController(for: question,
-                                      options: options, answerCallback: answerCallback)
-        
+        return questionViewController(for: question, options: options, answerCallback: answerCallback)
     }
     
     private func questionViewController(for question: Question<String>, options: [String], answerCallback: @escaping ([String]) -> Void) -> UIViewController {
@@ -63,7 +62,7 @@ class iOSViewControllerFactory: ViewControllerFactory {
     
     func resultViewController(for result: Results<Question<String>, [String]>) -> UIViewController {
         let presenter = ResultsPresenter(userAnswers: questions.map { ($0, result.answers[$0]!) },
-                                         correctAnswers: correctAnswers(),
+                                         correctAnswers: correctAnswers,
                                          scorer: { _, _ in result.score })
         let controller = ResultsViewController(summary: presenter.summary, answers: presenter.presentableAnswers)
         controller.title = presenter.title
@@ -72,7 +71,7 @@ class iOSViewControllerFactory: ViewControllerFactory {
     
     func resultViewController(for userAnswers: Answers) -> UIViewController {
         let presenter = ResultsPresenter(userAnswers: userAnswers,
-                                         correctAnswers: correctAnswers(),
+                                         correctAnswers: correctAnswers,
                                          scorer: BasicScore.score)
         let controller = ResultsViewController(summary: presenter.summary, answers: presenter.presentableAnswers)
         controller.title = presenter.title
