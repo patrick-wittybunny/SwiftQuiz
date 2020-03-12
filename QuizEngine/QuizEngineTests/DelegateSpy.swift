580b8ff838862b7ffa74b1fd203ab751a77acfb8
Patrick <patrick.wittybunny@gmail.com>
2020-03-12 14:08:45 +0800

    removed duplication by sharing delegate spy

---
+++ QuizEngine/QuizEngineTests/DelegateSpy.swift
@@ -0,0 +1,26 @@
+//
+//  DelegateSpy.swift
+//  QuizEngineTests
+//
+//  Created by Patrick Domingo on 3/12/20.
+//  Copyright © 2020 Patrick Domingo. All rights reserved.
+//
+
+import Foundation
+import QuizEngine
+
+class DelegateSpy: QuizDelegate {
+       var questionsAsked: [String] = []
+       var answerCompletions: [(String) -> Void] = []
+
+       var completedQuizzes: [[(String, String)]] = []
+       
+       func answer(for question: String, completion: @escaping (String) -> Void) {
+           questionsAsked.append(question)
+           answerCompletions.append(completion)
+       }
+       
+       func didCompleteQuiz(withAnswers answers: [(question: String, answer: String)]) {
+           completedQuizzes.append(answers)
+       }
+   }
