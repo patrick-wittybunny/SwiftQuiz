cf30dac2281f98e0ead0d25422c74f41971eb893
Patrick <patrick.wittybunny@gmail.com>
2020-03-12 11:17:20 +0800

    deprecated handle result and added a did complete quiz api

--- QuizEngine/QuizEngine/QuizDelegate.swift
+++ QuizEngine/QuizEngine/QuizDelegate.swift
@@ -16,7 +16,14 @@ public protocol QuizDelegate {
     associatedtype Answer
     
     func answer(for question: Question, completion: @escaping (Answer) -> Void)
+    func didCompleteQuiz(withAnswers: [(question: Question, answer: Answer)])
+    
+    @available(*, deprecated, message: "use didComplete(withAnswers:) instead")
     func handle(result: Results<Question, Answer>)
+}
+
+public extension QuizDelegate {
+    func didCompleteQuiz(withAnswers: [(question: Question, answer: Answer)]) {}
 }
 
 public protocol QuizResultBuilder {
